e286dc9caa161680b60de9d1e889b00282e6d0f5
kawalpreetkour <kawalpreetkour08@gmail.com>
2025-06-29 03:17:16 +0530

    Update backend.tf

--- sonarqube/backend.tf
+++ sonarqube/backend.tf
@@ -1,7 +1,9 @@
-backend "s3" {
+terraform {
+  backend "s3" {
     bucket         = "kawal-sonarqube-tfstate-bucket"
     key            = "sonarqube/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
+}
